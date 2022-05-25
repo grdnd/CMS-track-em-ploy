da35cd7efeb950b6c8026256abd6e3887d66a9c1
Gordo <gordonhdo@gmail.com>
2022-05-25 15:46:27 -0700

    creating initial structure

---
+++ index.js
@@ -0,0 +1,14 @@
+// import modules
+const { prompt } = require("prompt");
+
+// import database
+const db = require("./db");
+
+// create a function to initialize application
+function init() {
+  menu();
+}
+
+function menu() {}
+
+init();
